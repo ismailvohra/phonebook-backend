e3cb1de86b6d13998fe0717a4d4f73e1db8c2e2e
Ismail Vohra <ismail.voh@gmail.com>
2024-01-06 06:09:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -84,29 +84,6 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
